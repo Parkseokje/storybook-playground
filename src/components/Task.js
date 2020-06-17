faadbde3863f5276372ead4463772b4558f14f64
박석제 (Seokje Park) <seokje.park@gentlepie.com>
2020-06-18 01:43:38 +0900

    yarn storybook fails with index.css.. I don't know why :(

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export default function Task({
   task: { id, title, state },
-  onArchieveTask,
+  onArchiveTask,
   onPinTask,
 }) {
   return (
@@ -17,7 +17,7 @@ export default function Task({
         />
         <span
           className="checkbox-custom"
-          onClick={() => onArchieveTask(id)}
+          onClick={() => onArchiveTask(id)}
         ></span>
       </label>
       <div className="title">
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+// import WebFont from "webfontloader";
+
+// WebFont.load({
+//   google: {
+//     families: ["Nunito+Sans:400,400i,800", "sans-serif"],
+//   },
+// });
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
